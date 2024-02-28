070ed2af77cc3e06b365b6b4ed339e91657eb96d
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-02-28 16:21:17 +0100

    feat: generate_pwd function

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php
 
-
-
+function generate_pwd($pdw_length, $characters)
+{
+    $pwd_generated = "";
+    while (strlen($pwd_generated) < $pdw_length) {
+        $random_index = rand(0, 93);
+        for ($i = 0; $i <= strlen($characters); $i++) {
+            if ($i == $random_index) {
+                echo '"' . $characters[$i] . '" ';
+                $pwd_generated .= $characters[$i];
+            }
+            ;
+        }
+    }
+    return $pwd_generated;
+}
+;
 
 $ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz';
 $ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 $digits = '0123456789';
 $punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 $characters = $ascii_lowercase . $ascii_uppercase . $digits . $punctuation;
+var_dump($characters);
 
 $form_sent = !empty($_GET);
-$pwd_generated = "";
 
 
 ;
 if ($form_sent) {
     $pdw_length = (int) $_GET["pdw_length"];
-    while (strlen($pwd_generated) < $pdw_length) {
-        $random_index = rand(0, 94);
-        for ($i = 0; $i <= strlen($characters); $i++) {
-            if ($i == $random_index) {
-                echo $characters[$i] . "--- ";
-                $pwd_generated .= $characters[$i];
-            }
-            ;
-        }
-    }
-    var_dump($pwd_generated);
-    var_dump(strlen($pwd_generated));
+    $pwd = generate_pwd($pdw_length, $characters);
+    var_dump($pwd);
+    var_dump(strlen($pwd));
 }
 
 
@@ -51,7 +56,7 @@ if ($form_sent) {
         <button>Genera PDW</button>
     </form>
     <div> La PWD genereta è:
-        <?= $pwd_generated ?>
+        <?= $pwd ?>
     </div>
 </body>
 
